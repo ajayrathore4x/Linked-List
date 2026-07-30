5f61fa58067090c032aaa148b2006952ef719244
ajayrathore4x <ajayx0094@gmail.com>
2026-07-30 08:57:45 +0530

    detect a cycle

--- link.cpp
+++ link.cpp
@@ -180,6 +180,22 @@ Node* findMiddle(Node* head) {
 
     return slow;
 }
+bool detectCycle(Node* head){
+    Node* slow = head;
+Node* fast = head;
+
+while (fast != NULL && fast->next != NULL) {
+
+    slow = slow->next;
+    fast = fast->next->next;
+
+    if (slow == fast) {
+        return true;
+    }
+}
+
+return false;
+}
 int main(){
      Node* head = new Node(10);
 
