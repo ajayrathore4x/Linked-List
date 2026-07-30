78df9167138d4d4cb917780c573725786a64dd91
ajayrathore4x <ajayx0094@gmail.com>
2026-07-30 08:54:40 +0530

    to get first middle in even linked list

--- link.cpp
+++ link.cpp
@@ -173,7 +173,7 @@ Node* findMiddle(Node* head) {
     Node* slow = head;
     Node* fast = head;
 
-    while(fast != NULL && fast->next != NULL) {
+    while(fast != NULL && fast->next != NULL&& fast->next->next!=NULL) {
         slow = slow->next;
         fast = fast->next->next;
     }
